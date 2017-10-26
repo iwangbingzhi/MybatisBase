582ee79784d6b93cec0b125e87cd76532c99452a
wangbingzhigit <853244870@qq.com>
2017-10-26 21:01:01 +0800

    1

--- src/com/wbz/mapper/UserMapper.java
+++ src/com/wbz/mapper/UserMapper.java
@@ -13,6 +13,15 @@ mapper开发规范：
 
 import com.wbz.po.User;
 
+import java.util.List;
+
 public interface UserMapper {
+    //根据id查询用户
     public User findUserById(int id) throws Exception;
+    //插入用户
+    public void insertUser(User user) throws Exception;
+    //删除用户
+    public void deleteUser(int id) throws Exception;
+    //根据用户名查询用户列表
+    public List<User> findUserByName(String name) throws Exception;
 }
--- src/com/wbz/mapper/UserMapperTest.java
+++ src/com/wbz/mapper/UserMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -31,7 +32,21 @@ public class UserMapperTest {
 
         //调用userMapper方法
         User user = userMapper.findUserById(5);
-        System.out.println(user);
 
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testfindUserByName() throws Exception {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //创建UserMapper对象，mybatis自动生成mapper代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        //调用userMapper方法
+        List<User> list = userMapper.findUserByName("小");
+        System.out.println(list);
+        sqlSession.close();
     }
 }
