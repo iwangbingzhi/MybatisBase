bb17cc479a5ec158554491df8f94092376d9e5fe
wangbingzhigit <853244870@qq.com>
2017-10-26 15:09:36 +0800

    1

---
+++ src/com/wbz/po/User.java
@@ -0,0 +1,62 @@
+package com.wbz.po;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String username;
+    private String sex;
+    private Date birthday;
+    private String adress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+}
