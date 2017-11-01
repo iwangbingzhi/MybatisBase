a0425f55366f939087047fc5bea073fd348d3cc5
wangbingzhigit <853244870@qq.com>
2017-11-01 21:51:23 +0800

    1

---
+++ src/com/wbz/mapper/OrdersMapperCustomer.java
@@ -0,0 +1,18 @@
+package com.wbz.mapper;
+
+import com.wbz.po.Orderdetail;
+import com.wbz.po.Orders;
+import com.wbz.po.OrdersCustomer;
+
+import java.util.List;
+
+public interface OrdersMapperCustomer {
+    //查询订单关联用户信息
+    public List<OrdersCustomer> findOrdersUser() throws Exception;
+
+    //resultmap查询订单关联用户信息
+    public List<Orders> findOrdersUserResultMap() throws Exception;
+
+    //resultmap配置订单明细信息
+    public List<Orderdetail> findOrdersAndOrderDetailResultMap() throws Exception;
+}
---
+++ src/com/wbz/mapper/OrdersMapperCustomerTest.java
@@ -0,0 +1,53 @@
+package com.wbz.mapper;
+
+import com.wbz.po.Orders;
+import com.wbz.po.OrdersCustomer;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrdersMapperCustomerTest {
+    private SqlSessionFactory sqlSessionFactory;
+    @Before
+    public void setUp() throws Exception {
+        String resource = "com/wbz/config/SqlMapConfig.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+
+    @Test
+    public void testFindOrdersUser() throws Exception {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //创建代理对象
+        OrdersMapperCustomer ordersMapperCustomer = sqlSession.getMapper(OrdersMapperCustomer.class);
+        //调用mapper的方法
+        List<OrdersCustomer> list = ordersMapperCustomer.findOrdersUser();
+
+        System.out.println(list);
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindOrdersUserResultMap() throws Exception {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //创建代理对象
+        OrdersMapperCustomer ordersMapperCustomer = sqlSession.getMapper(OrdersMapperCustomer.class);
+        //调用mapper的方法
+        List<Orders> list = ordersMapperCustomer.findOrdersUserResultMap();
+
+        System.out.println(list);
+        sqlSession.close();
+    }
+
+
+}
